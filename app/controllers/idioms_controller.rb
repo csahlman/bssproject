d4b625aad4434311a879030fb6be9420b92a52e9
Charlie Sahlman <csahlman@gmail.com>
2012-11-19 20:51:18 -0500

    Added styling and html to fix the ajax on show for tags

--- app/controllers/idioms_controller.rb
+++ app/controllers/idioms_controller.rb
@@ -44,6 +44,8 @@ class IdiomsController < ApplicationController
   def show
     @idiom= Idiom.find(params[:id])
     @vote= @idiom.votes.find_or_initialize_by_user_id(current_user.id)
+    @tag_for_voting = @idiom.random_tag
+    @tag_vote = @tag_for_voting.votes.find_or_initialize_by_user_id(current_user.id)
     respond_to do |f|
       f.html
       f.json { render json: @idiom}
