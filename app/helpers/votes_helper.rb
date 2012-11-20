84e6a46663e4d6675d4f4706cbff93a4552c5c89
Charlie Sahlman <csahlman@gmail.com>
2012-11-20 11:14:42 -0500

    Added styling to color votes

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,10 @@
 module VotesHelper
+
+  def proper_score(vote)
+    if vote.voteable.total_score >= 0
+      content_tag :span, vote.voteable.total_score, class: "positive"
+    else
+      content_tag :span, vote.voteable.total_score, class: 'negative'
+    end
+  end
 end
