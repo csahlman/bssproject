8655303a497e0830055483d4cf0d9142206fcbbe
Charlie Sahlman <csahlman@gmail.com>
2012-11-26 14:02:20 -0500

    styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,16 @@ class UsersController < ApplicationController
   respond_to :html, :js, :json
 
   def new
-    @user= User.new
+    @user = User.new
   end
 
   def create
-    @user= User.new
-    @user.email= params[:user][:email]
-    @user.name= params[:user][:name]
-    @user.password= params[:user][:password]
-    @user.password_confirmation= params[:user][:password_confirmation]
-    @user.about_me= params[:user][:about_me]
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.name = params[:user][:name]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    @user.about_me = params[:user][:about_me]
     if @user.save
       sign_in(@user)
       respond_with @user do |f|
@@ -29,11 +29,11 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users= User.all
+    @users = User.all
   end
 
   def show
-    @user= User.find(params[:id])
+    @user = User.find(params[:id])
     @edits_count = Edit.where(user_id: @user).count
   end
 
@@ -45,11 +45,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.email= params[:user][:email]
-    @user.name= params[:user][:name]
-    @user.password= params[:user][:password]
-    @user.password_confirmation= params[:user][:password_confirmation]
-    @user.about_me= params[:user][:about_me]
+    @user.email = params[:user][:email]
+    @user.name = params[:user][:name]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    @user.about_me = params[:user][:about_me]
     if @user.save
       respond_with @user do |f|
         f.html { redirect_to @user, flash: { notice: "#{@user.name} successfully updated"} }
@@ -64,8 +64,8 @@ class UsersController < ApplicationController
 
   private
     def must_be_own_profile_or_admin
-      @user= User.find(params[:id])
+      @user = User.find(params[:id])
       redirect_to root_path, flash: { error: "You don't have access to that" } unless
-        @user== current_user || current_user.admin?
+        @user == current_user || current_user.admin?
     end
 end
