a26b0c8ffe8901a45e78072da3789cea09dd94aa
Charlie Sahlman <csahlman@gmail.com>
2012-12-03 20:13:59 -0500

    Altered a faulty migration

--- db/migrate/20121120210116_remove_sender_id_and_receiver_id_from_conversations.rb
+++ db/migrate/20121120210116_remove_sender_id_and_receiver_id_from_conversations.rb
@@ -1,7 +1,7 @@
 class RemoveSenderIdAndReceiverIdFromConversations < ActiveRecord::Migration
   def up
-    remove_column :conversations, :sender_id, :integer
-    remove_column :conversations, :receiver_id, :integer
+    remove_column :conversations, :sender_id
+    remove_column :conversations, :receiver_id
   end
 
   def down
